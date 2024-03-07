aae158d426827d16b51b561faee8fe115eb98036
Etienne Prud’homme <e.e.f.prudhomme@gmail.com>
2024-03-07 11:43:50 -0500

    Tweak env variable to force dark theme for GTK
    * .shell/env.d/dark_theme.sh: Use environment variables to force using
    GTK theme with an Ubuntu bug.

---
+++ .shell/env.d/dark_theme.sh
@@ -0,0 +1,8 @@
+# -*- mode: sh; sh-shell: sh -*-
+# vim:ft=sh:
+
+# /bin/echo -e '\033[1;32mIn .shell/env.d/dark_theme.sh\033[0m'
+
+# https://askubuntu.com/a/1464952/1079926
+export ADW_DEBUG_COLOR_SCHEME=prefer-dark
+export GTK_THEME=Yaru:dark
